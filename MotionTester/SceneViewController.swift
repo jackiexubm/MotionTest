7e1b4258543b072c6ac6b2bfef6b38d3b5a13448
Jackie Xu <jackiexubm1@gmail.com>
2017-04-29 03:37:27 -0400

    Complete height and walk buttons

--- MotionTester/SceneViewController.swift
+++ MotionTester/SceneViewController.swift
@@ -12,6 +12,12 @@ import CoreMotion
 
 class SceneViewController: UIViewController, SCNSceneRendererDelegate{
     
+    override var prefersStatusBarHidden: Bool {
+        get {
+            return true
+        }
+    }
+    
     var gameView: SCNView!
     var gameScene: SCNScene!
     var cameraNode: SCNNode!
@@ -23,6 +29,8 @@ class SceneViewController: UIViewController, SCNSceneRendererDelegate{
     var testNode: SCNNode!
     
     var transformTimer: TimeInterval = 0
+    weak var walkTimer: Timer?
+    weak var heightTimer: Timer?
     
     let motionManager: MyMotion = MyMotion()
     
@@ -30,10 +38,11 @@ class SceneViewController: UIViewController, SCNSceneRendererDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         initView()
         initScene()
         initCamera()
-        //        initLabels()
+        initSubviews()
         createObject()
         
         motionManager.getAttitudeAsync(interval: 0.05) { (attitude) in
@@ -92,8 +101,6 @@ class SceneViewController: UIViewController, SCNSceneRendererDelegate{
         
         //       cameraNode.camera?.xFov = 45
         //        cameraNode.camera?.yFov = 90
-        
-        
     }
     
     func createObject(){
@@ -143,31 +150,16 @@ class SceneViewController: UIViewController, SCNSceneRendererDelegate{
         
     }
     
-    //    func initLabels(){
-    //        view.addSubview(Roll)
-    //        view.addSubview(Pitch)
-    //        view.addSubview(Yaw)
-    //        view.addSubview(RollVal)
-    //        view.addSubview(PitchVal)
-    //        view.addSubview(YawVal)
-    //
-    //        addConstraintString(str: "H:|-5-[v0][v1(55)]-5-[v2][v3(55)]-5-[v4][v5(55)]", views: ["v0": Roll, "v1": RollVal, "v2": Pitch, "v3": PitchVal, "v4": Yaw, "v5": YawVal])
-    //        addConstraintString(str: "V:[v0]-2-|", views: ["v0":Roll])
-    //        addConstraintString(str: "V:[v0]-2-|", views: ["v0":RollVal])
-    //        addConstraintString(str: "V:[v0]-2-|", views: ["v0":Pitch])
-    //        addConstraintString(str: "V:[v0]-2-|", views: ["v0":PitchVal])
-    //        addConstraintString(str: "V:[v0]-2-|", views: ["v0":Yaw])
-    //        addConstraintString(str: "V:[v0]-2-|", views: ["v0":YawVal])
-    //    }
+
     
-    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
-        if time > transformTimer{
-            //cameraNode.position.y += 0.3
-            testNode.eulerAngles.z += 0.005
-            //                objNode.eulerAngles.z += Float(5 * M_PI / 180)
-            transformTimer = time + 0.1
-        }
-    }
+//    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+//        if time > transformTimer{
+//            //cameraNode.position.y += 0.3
+//            testNode.eulerAngles.z += 0.005
+//            //                objNode.eulerAngles.z += Float(5 * M_PI / 180)
+//            transformTimer = time + 0.1
+//        }
+//    }
     
     //    override var shouldAutorotate: Bool {
     //        return false
@@ -222,17 +214,142 @@ class SceneViewController: UIViewController, SCNSceneRendererDelegate{
     //        return view
     //    }()
     //
-    //    func addConstraintString(str: String, views: [String: UIView]){
-    //        self.view.addConstraints(NSLayoutConstraint.constraints(
-    //            withVisualFormat: str,
-    //            options: NSLayoutFormatOptions(),
-    //            metrics: nil,
-    //            views: views)
-    //        )
-    //
-    //}
     
     
+    func addConstraintString(str: String, views: [String: UIView]){
+            self.view.addConstraints(NSLayoutConstraint.constraints(
+                withVisualFormat: str,
+                options: NSLayoutFormatOptions(),
+                metrics: nil,
+                views: views)
+            )
+    
+    }
+    
+    func initSubviews(){
+        view.addSubview(walkButtonsImage)
+        view.addSubview(heightButtonsImage)
+        view.addSubview(upAisleButton)
+        view.addSubview(downAisleButton)
+        view.addSubview(raiseCameraButton)
+        view.addSubview(lowerCameraButton)
+        
+        addConstraintString(str: "H:|-10-[v0(100)]", views: ["v0": walkButtonsImage])
+        addConstraintString(str: "V:|-10-[v0(50)]", views: ["v0": walkButtonsImage])
+
+        addConstraintString(str: "H:|-10-[v0(100)]", views: ["v0": heightButtonsImage])
+        addConstraintString(str: "V:[v0(50)]-10-|", views: ["v0": heightButtonsImage])
+        
+        addConstraintString(str: "H:|[v0(60)][v1(60)]", views: ["v0": downAisleButton, "v1": upAisleButton])
+        addConstraintString(str: "V:|[v0(70)]", views: ["v0": upAisleButton])
+        addConstraintString(str: "V:|[v0(70)]", views: ["v0": downAisleButton])
+        
+        addConstraintString(str: "H:|[v0(60)][v1(60)]", views: ["v0": lowerCameraButton, "v1": raiseCameraButton])
+        addConstraintString(str: "V:[v0(70)]|", views: ["v0": raiseCameraButton])
+        addConstraintString(str: "V:[v0(70)]|", views: ["v0": lowerCameraButton])
+
+
+        
+    }
+    
+    let walkButtonsImage: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = #imageLiteral(resourceName: "WalkButtons")
+        return view
+    }()
+    
+    let heightButtonsImage: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = #imageLiteral(resourceName: "HeightButtons")
+        return view
+    }()
+    
+    let upAisleButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(startWalkUp), for: UIControlEvents.touchDown)
+        button.addTarget(self, action: #selector(stopWalkUp), for: UIControlEvents.touchUpInside)
+        button.addTarget(self, action: #selector(stopWalkUp), for: UIControlEvents.touchUpOutside)
+        button.addTarget(self, action: #selector(stopWalkUp), for: UIControlEvents.touchDragExit)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    let downAisleButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(startWalkDown), for: UIControlEvents.touchDown)
+        button.addTarget(self, action: #selector(stopWalkDown), for: UIControlEvents.touchUpInside)
+        button.addTarget(self, action: #selector(stopWalkDown), for: UIControlEvents.touchUpOutside)
+        button.addTarget(self, action: #selector(stopWalkDown), for: UIControlEvents.touchDragExit)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let raiseCameraButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(startRaiseCamera), for: UIControlEvents.touchDown)
+        button.addTarget(self, action: #selector(stopRaiseCamera), for: UIControlEvents.touchUpInside)
+        button.addTarget(self, action: #selector(stopRaiseCamera), for: UIControlEvents.touchUpOutside)
+        button.addTarget(self, action: #selector(stopRaiseCamera), for: UIControlEvents.touchDragExit)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let lowerCameraButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(startLowerCamera), for: UIControlEvents.touchDown)
+        button.addTarget(self, action: #selector(stopLowerCamera), for: UIControlEvents.touchUpInside)
+        button.addTarget(self, action: #selector(stopLowerCamera), for: UIControlEvents.touchUpOutside)
+        button.addTarget(self, action: #selector(stopLowerCamera), for: UIControlEvents.touchDragExit)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+
+    
+    func startRaiseCamera(){
+        heightTimer = Timer.scheduledTimer(withTimeInterval: 0.06, repeats: true) { (_) in
+            self.cameraNode.position.z += 0.02
+        }
+    }
+    
+    func stopRaiseCamera(){
+        heightTimer?.invalidate()
+    }
+    
+    func startLowerCamera(){
+        heightTimer = Timer.scheduledTimer(withTimeInterval: 0.06, repeats: true) { (_) in
+            self.cameraNode.position.z -= 0.02
+        }
+    }
+    
+    func stopLowerCamera(){
+        heightTimer?.invalidate()
+    }
+    
+    func startWalkUp(){
+        walkTimer = Timer.scheduledTimer(withTimeInterval: 0.06, repeats: true) { (_) in
+            self.cameraNode.position.y += 0.09
+        }
+    }
+    
+    func stopWalkUp(){
+        walkTimer?.invalidate()
+    }
+    
+    func startWalkDown(){
+        walkTimer = Timer.scheduledTimer(withTimeInterval: 0.06, repeats: true) { (_) in
+            self.cameraNode.position.y -= 0.09
+        }
+    }
+    
+    func stopWalkDown(){
+        walkTimer?.invalidate()
+    }
+    
+    
+    
+
     
     
     
